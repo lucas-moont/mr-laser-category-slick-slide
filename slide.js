e2c7b8504800c5d62c4222fc7b9a38347b25e30a
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-11-19 16:08:58 -0300

    --feat Adding the slick slider.

--- slide.js
+++ slide.js
@@ -1,8 +1,26 @@
-$(document).ready(function(){
-  $('.category-slide-inner').slick({
+$(document).ready(function () {
+  $(".category-slide-inner").slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     speed: 400,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: `
+      <button class='slideBtn prevBtn'>
+        <img src='https://mrlaser.com.br/wp-content/uploads/2024/11/Vector-7.png'>
+      </button>  
+    `,
+    nextArrow:'',
+    responsive: [
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 400,
+        },
+      },
+    ],
   });
 });
